c40cb253e70616d4ab8ff7256bca3f853adad693
k4m4 <nikolaskam@gmail.com>
2020-05-25 19:51:10 +0300

    Add SHA-0 and SHA-3 support (closes #1)

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ const hashPerBits = {
   52: ['FNV-1a-52'],
   64: ['FNV-1-64', 'FNV-1a-64'],
   128: ['MD2', 'MD4', 'MD5', 'MD6', 'RIPEMD-180', 'Tiger-128', 'Snerfu-128', 'FNV-1a-128', 'MDC-2', 'HAVAL-128'],
-  160: ['SHA-1', 'RIPEMD', 'Tiger', 'HAVAL-160', 'HAS-160'],
+  160: ['SHA-0', 'SHA-1', 'RIPEMD', 'Tiger', 'HAVAL-160', 'HAS-160'],
   192: ['Tiger-192', 'HAVAL-192'],
-  224: ['SHA-224', 'HAVAL-224'],
-  256: ['SHA-256', 'RIPEMD-256', 'GOST', 'Snerfu-256', 'Streebog-256', 'HAVAL-256'],
+  224: ['SHA-224', 'SHA3-224', 'HAVAL-224'],
+  256: ['SHA-256', 'SHA3-256', 'RIPEMD-256', 'GOST', 'Snerfu-256', 'Streebog-256', 'HAVAL-256'],
   320: ['RIPEMD-320'],
-  384: ['SHA-384'],
-  512: ['SHA-512', 'FNV-1a-512', 'BLAKE-512', 'Whirpool', 'Spectral Hash', 'Streebog-512'],
+  384: ['SHA-384', 'SHA3-384'],
+  512: ['SHA-512', 'SHA3-512', 'FNV-1a-512', 'BLAKE-512', 'Whirpool', 'Spectral Hash', 'Streebog-512'],
   1024: ['FNV-1a-1024']
 }
 
@@ -24,7 +24,7 @@ function detectHash (hash) {
   if (bits in hashPerBits) {
     return hashPerBits[bits];
   } else {
-    return 'Hash type could not be detected'
+    return 'Hash type could not be detected';
   }
 }
 
